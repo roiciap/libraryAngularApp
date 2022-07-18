579ec004c653deeb0cd4ac3b04794ca2c3f0cebe
roiciap <roizab2000@gmail.com>
2022-07-18 11:15:29 +0200

    'wypozyczenia'

--- src/app/wypozyczenia/wypozyczenia.component.ts
+++ src/app/wypozyczenia/wypozyczenia.component.ts
@@ -1,7 +1,6 @@
 // todo: nie używane importy
 
 import { Component, OnInit } from '@angular/core';
-import { Wypozyczenie } from 'src/Types/Wypozyczenie';
 import { LoanDescription } from 'src/Types/LoanDescription';
 import { LoansService } from '../services/loans/loans.service';
 
@@ -17,35 +16,25 @@ export class WypozyczeniaComponent implements OnInit {
   // todo po co _
   constructor(private loansService: LoansService) {}
 
-  loans: Array<Wypozyczenie> = [];
-
-  loansWD: Array<LoanDescription> = [];
+  loans: Array<LoanDescription> = [];
+  peopleWithLoans: number = 0;
 
   ngOnInit(): void {
-    this.getLoansDescription(true);
-  }
-  ksiazkaInput: number = 0;
-  osobaInput: number = 0;
-
-  // todo: zwracany typ
-  submit(): void {
-    this.loansService.addLoan({
-      idKsiazka: this.ksiazkaInput,
-      idOsoba: this.osobaInput,
-    });
-  }
-
-  getLoans(): void {
-    this.loansService
-      .getAllLoans()
-      .subscribe((data) => (this.loans = [...data]));
+    this.getLoansDescription();
   }
 
   // todo: jak nie uzywane to wyjebanc
-  getLoansDescription(returned: boolean = false): void {
+  getLoansDescription(): void {
     this.loansService
       .getLoansDetails({ returned: false })
-      .subscribe((data) => (this.loansWD = data));
+      .subscribe(
+        (data) => (
+          (this.loans = data),
+          (this.peopleWithLoans = new Set(
+            data.map((loan) => loan.Person.id)
+          ).size)
+        )
+      );
   }
   returnBook(loanID: number): void {
     this.loansService.returnBook(loanID);
