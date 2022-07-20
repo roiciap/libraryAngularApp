c2c3b27cbe9fd28293a0301d44d7e2d58ff75c72
MoSzynMTS <szmsmi@gmail.com>
2022-07-20 12:32:21 +0200

    removed ids from book

--- src/app/services/utils/string-utils.service.ts
+++ src/app/services/utils/string-utils.service.ts
@@ -3,7 +3,6 @@ import { PageName } from 'src/app/enums/page-name.enum';
 export class StringUtilsService {
   capitWord(word: string): string {
     if (word.length > 0) {
-      // todo: czemu wordRep jest let jak nie zmieniasz nigdzie
       const wordRep = word.replace(/[/[0-9]/g, '');
       return `${wordRep[0].toUpperCase()}${wordRep.slice(1).toLowerCase()}`;
     }
